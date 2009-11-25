d6054fc830e723c20e5c2d915f794ff479c1855b
aschild <aschild@6541b307-c4c0-ebbd-d50e-d7e3313f818b>
2009-11-25 17:56:57 +0000

    Applied patch to make KeystrokeTextField work again with Echo Beta8 and later releases. https://echopoint.dev.java.net/issues/show_bug.cgi?id=10
    git-svn-id: https://svn.java.net/svn/echopoint~svn/trunk@254 6541b307-c4c0-ebbd-d50e-d7e3313f818b

--- src/client/js/Sync.KeystrokeTextField.js
+++ src/client/js/Sync.KeystrokeTextField.js
@@ -10,23 +10,20 @@ echopoint.KeystrokeTextFieldSync = Core.extend( echopoint.internal.TextFieldSync
   },
 
   /** The keyUp function implementation. */
-  processKeyUp: function( event )
+  clientKeyUp : function( event )
   {
     //var newText = this.component.get("text");
     //Core.Debug.consoleWrite("text in keyUpEvent is: "+newText);
     //Core.Debug.consoleWrite("input.value in keyUpEvent is: "+this.input.value);
 
     event = (event) ? event : window.event;
-    if (event.keyCode != 13)
+    if(event.keyCode != 13)
     {
-            var keystrokeDelay = this.component.render("keystrokeDelay", 0);
-            if (keystrokeDelay == 0)
-            {
-                    this.executeSync();
-            } else if (keystrokeDelay > 0)
-            {
-                    this.scheduleSync(keystrokeDelay);
-            }
+      var keystrokeDelay = this.component.render("keystrokeDelay", 0);
+      if(keystrokeDelay > 0)
+        this.scheduleSync(keystrokeDelay);
+      else
+        this.executeSync();
     }
     return true;
   },
@@ -36,49 +33,57 @@ echopoint.KeystrokeTextFieldSync = Core.extend( echopoint.internal.TextFieldSync
     echopoint.internal.TextFieldSync.call( this );
   },
 
-    scheduleSync : function(delay) {
-            // Cancel pending sync before a new sync is scheduled,
-            // so only the last sync in a row is executed
-            // while someone is typing rapidly.
-            // Core.Debug.consoleWrite("Schedulesync with "+delay+"ms");
-            this.cancelPendingSync();
-            this.pendingSync = Core.Web.Scheduler.run(Core.method(this,
-                                            this.executeSync), delay, false);
-    },
+  scheduleSync : function(delay) 
+  {
+    // Cancel pending sync before a new sync is scheduled,
+    // so only the last sync in a row is executed
+    // while someone is typing rapidly.
+    // Core.Debug.consoleWrite("Schedulesync with "+delay+"ms");
+    this.cancelPendingSync();
+    this.pendingSync = Core.Web.Scheduler.run(Core.method(this, this.executeSync), delay, false);
+  },
 
-    cancelPendingSync : function() {
-            if (this.pendingSync !== undefined) {
-                    Core.Web.Scheduler.remove(this.pendingSync);
-                    delete this.pendingSync;
-            }
-    },
-
-    executeSync : function() {
-          if (this.component != null)
-          {
-            // Since the echo base text component stores the input value only
-            // Before processing events, we have to force it do be done here.
-            // Otherwise our doAction command will be processed without the last
-            // updates to the text field. (Usullay last character entered missing)
-            this._storeValue();
-
-            var newText = this.input.value;
-            if (this.oldText != newText) {
-                    this.oldText = newText;
-                    this.component.doAction();
-            }
-            else {
-                // Core.Debug.consoleWrite("Execsync text not changed");
-            }
-          }
-          else {
-                // Core.Debug.consoleWrite("Execsync component is null (removed from dom?)");
-          }
-    },
-
-    // Cancel any pending syncs on dispose
-    renderDispose : function() {
-            this.cancelPendingSync();
+  cancelPendingSync : function() 
+  {
+    if (this.pendingSync !== undefined) 
+    {
+      Core.Web.Scheduler.remove(this.pendingSync);
+      delete this.pendingSync;
     }
+  },
 
+  executeSync : function() 
+  {
+    if(this.component != null)
+    {
+      // Since the echo base text component stores the input value only
+      // Before processing events, we have to force it do be done here.
+      // Otherwise our doAction command will be processed without the last
+      // updates to the text field. (Usullay last character entered missing)
+      var newText = this.input.value;
+      var oldText = this._lastProcessedValue;
+    
+      this._storeSelection();
+      this._storeValue(null);
+    
+      if(oldText != newText) 
+      {
+        this.component.doAction();
+      }
+      else 
+      {
+        // Core.Debug.consoleWrite("Execsync text not changed");
+      }
+    }
+    else 
+    {
+      // Core.Debug.consoleWrite("Execsync component is null (removed from dom?)");
+    }
+  },
+
+  // Cancel any pending syncs on dispose
+  renderDispose : function() 
+  {
+    this.cancelPendingSync();
+  }
 });
